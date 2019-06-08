0dcb5cb75aa4c39d8552548d6145f363d5cbd4b2
Zac Smith <zrs3141592@gmail.com>
2019-06-08 17:24:26 -0600

    added loading notice

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      loading: false
     };
   }
 
@@ -18,12 +19,18 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
+    this.setState({
+      loading: true
+    });
     fetch(URL)
       .then(res => {
         return res.json();
       })
       .then(data => {
         this.setState({ starwarsChars: data.results });
+        this.setState({
+          loading: false
+        });
       })
       .catch(err => {
         throw new Error(err);
@@ -34,15 +41,21 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        {this.state.starwarsChars.map(char => (
-          <div className="display">
-            <ul>
-              <li id="char-name">{`${char.name}`}</li>
-              <li>{`Height: ${char.height}cm Weight: ${char.mass}kg`}</li>
-              <li>{`Birth Year: ${char.birth_year}`}</li>
-            </ul>
+        {!this.state.loading ? (
+          this.state.starwarsChars.map(char => (
+            <div className="display">
+              <ul>
+                <li id="char-name">{`${char.name}`}</li>
+                <li>{`Height: ${char.height}cm Weight: ${char.mass}kg`}</li>
+                <li>{`Birth Year: ${char.birth_year}`}</li>
+              </ul>
+            </div>
+          ))
+        ) : (
+          <div className="loading">
+            <p>... Loading!</p>
           </div>
-        ))}
+        )}
         <NextBtnComponent />
       </div>
     );
